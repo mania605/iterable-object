797b653b4a28f4a1a47ef6550bdc0f468f07beec
mania605 <kim.minsun83@gmail.com>
2024-09-19 17:19:24 +0900

    텍스트 문자열 반복 생성 함수 미션 gpt도움

--- script.js
+++ script.js
@@ -18,4 +18,21 @@ for (let letter of text) {
 //step3 - 인터벌 시간값을 3번째 인수로 전달하면 delay값이 설정됨
 //step4 - 세번째 인수값이 전달되지 않으면 무조건 delay값을 디폴트로 0처리
 
-//splitText('h1','span', 0.1)
+
+
+// function splitText(selector, wrapperElement = 'span', delay = 0.2) {
+//   const element = document.querySelector(selector);
+//   const text = element.innerText;
+//   element.innerHTML = text.split('').map(letter => `<${wrapperElement}>${letter}</${wrapperElement}>`).join('');
+//   const spans = element.querySelectorAll(wrapperElement);
+
+//   element.addEventListener('mouseenter', () =>
+//     spans.forEach((span, i) => setTimeout(() => span.style.opacity = '0', i * delay * 1000))
+//   );
+
+//   element.addEventListener('mouseleave', () =>
+//     spans.forEach((span, i) => setTimeout(() => span.style.opacity = '1', i * delay * 1000))
+//   );
+// }
+
+// splitText('h1', 'span', 0.1);
